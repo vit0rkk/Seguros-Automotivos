81cca589152aafd4ab4a7200faac8f12050caec8
ceciliaferreira55 <ceciliaferreira@acad.ifma.edu.br>
2026-07-11 13:18:22 -0300

    segunda parte; editei o veiculo.c pra dar uma otimizada, fiz o condutor.h e condutor.c e editei o design

--- veiculo.c
+++ veiculo.c
@@ -16,38 +16,48 @@ static int validar_tipo_veiculo(char t) {
 }
 
 void cadastrar_veiculo(Veiculo *v) {
-    printf("=== CADASTRO DO VEICULO ===\n");
+    printf("\nCadastro do Veiculo\n");
     
     printf("Marca: ");
     fgets(v->marca, sizeof(v->marca), stdin);
-    v->marca[strcspn(v->marca, "\n")] = 0;
+    //limpar o resto que sobrou do buffer se a marca for maio que o array
+    if(v->marca[strcspn(v->marca, "\n")] == '\0' && strlen(v->marca) == sizeof(v->marca) - 1) {
+        limpar_buffer_veiculo();
+    } else {
+        v->marca[strcspn(v->marca, "\n")] = 0;
+    }
 
     printf("Modelo: ");
     fgets(v->modelo, sizeof(v->modelo), stdin);
-    v->modelo[strcspn(v->modelo, "\n")] = 0;
+    if(v->modelo[strcspn(v->modelo, "\n")] == '\0' && strlen(v->modelo) == sizeof(v->modelo) - 1) {
+        limpar_buffer_veiculo();
+    } else {
+        v->modelo[strcspn(v->modelo, "\n")] = 0;
+    }
 
     do {
         printf("Ano de fabricacao: ");
         if (scanf("%d", &v->ano) != 1 || v->ano < 1886) {
             printf("Erro: Ano invalido.\n");
-            limpar_buffer_veiculo();
             v->ano = 0;
         }
+        limpar_buffer_veiculo(); //mudei tanto aqui quanto no condutor
     } while (v->ano == 0);
 
     do {
         printf("Valor de mercado (R$): ");
         if (scanf("%f", &v->valor_mercado) != 1 || v->valor_mercado <= 0) {
             printf("Erro: Valor deve ser maior que zero.\n");
-            limpar_buffer_veiculo();
             v->valor_mercado = 0;
         }
+        limpar_buffer_veiculo();
     } while (v->valor_mercado <= 0);
 
     do {
         printf("Tipo [(P) Passeio / (U) Utilitario / (E) Esportivo]: ");
         scanf(" %c", &v->tipo);
         v->tipo = toupper(v->tipo);
+        limpar_buffer_veiculo();
         if (!validar_tipo_veiculo(v->tipo)) {
             printf("Erro: Tipo invalido! Digite P, U ou E.\n");
         }
